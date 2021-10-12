278b75efa686168b81e51066a10bf4f6ce5af0f9
MariusArdeleann <lilamc008@gmail.com>
2021-10-12 23:24:26 +0300

    settings design done

---
+++ src/pages/settings/settings.jsx
@@ -0,0 +1,36 @@
+import "./settings.css";
+import Sidebar from "../../components/sidebar/sidebar";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://i.ytimg.com/vi/6di34z0eAaI/maxresdefault.jpg"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>UserName</label>
+          <input type="text" placeholder="Marius" />
+          <label>Email</label>
+          <input type="email" placeholder="marius@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
